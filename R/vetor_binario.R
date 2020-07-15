ddb647865250b209399db4c459dc23178ab1d1b0
Natalia-SS <46005006+Natalia-SS@users.noreply.github.com>
2020-07-14 23:25:10 -0300

    Função vetores binários adicionado

---
+++ R/vetor_binario.R
@@ -0,0 +1,24 @@
+modelos_possiveis <- function(vet){
+
+  if(!require(dplyr)){ install.packages('dplyr'); library('dplyr')} else{ require(dplyr)}
+  if(!require(e1071)){ install.packages('e1071'); library('e1071')} else{ require(e1071)}
+
+  qnt_var <- length(vet)
+  tds_modelos <- bincombinations(qnt_var) # ate 22 variáveis
+
+  conte <- matrix(0,nrow(tds_modelos), ncol(tds_modelos))
+  for (i in 1:nrow(tds_modelos)) {
+    for (j in 1:ncol(tds_modelos)) {
+      if((vet[j]==1 & tds_modelos[i,j]==0) || (vet[j]==0 & tds_modelos[i,j]==1)){
+        conte[i,j] <- 1
+      }
+    }
+  }
+
+  ver <- as.data.frame(tds_modelos) %>%
+    filter(apply(conte,1,sum)==1)
+  return(ver)
+}
+
+
+
